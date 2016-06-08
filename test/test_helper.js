2423d5b984f4521c07aeacc95f006f191fbb0198
zmpeg <zandstra.matt@gmail.com>
2016-06-08 12:29:39 -0600

    include colorbrewer, return color scales

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,12 +1,13 @@
 var test = require('tape');
-var colorbrewer = require('../');
+var colorbrewify = require('../');
+var colorbrewer = require('colorbrewer');
 
 test('sequential test', function(t) {
     t.plan(1);
 
-    seq = [1,2,3,4,5,6];
+    actual = [1,2,3,4,5,6,3,2,3,6,1];
 
-    t.equal(colorbrewer(seq), 'sequential');
+    t.equal(colorbrewify(actual), colorbrewer.YlGn[3]);
 
 });
 
@@ -15,6 +16,6 @@ test('binary test', function(t) {
 
     bin = [true,false,true,true,false];
 
-    t.equal(colorbrewer(bin), 'binary');
+    t.equal(colorbrewify(bin), colorbrewer.YlGn[3]);
 
 });
