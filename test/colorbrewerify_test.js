ea0db1cf99b45147fc207ed83f5bc1c4ccfe306b
zmpeg <zandstra.matt@gmail.com>
2016-06-08 12:53:59 -0600

    do not attempt to colorbrewer bad data

--- test/colorbrewerify_test.js
+++ test/colorbrewerify_test.js
@@ -2,14 +2,24 @@ var test = require('tape');
 var colorbrewify = require('../');
 var colorbrewer = require('colorbrewer');
 
-test('sequential test', function(t) {
+test('it complains about badly formatted arrays', function(t) {
     t.plan(1);
 
-    actual = [1,2,3,4,5,6,3,2,3,6,1];
+    data = [1,{},false];
 
-    t.equal(colorbrewify(actual), colorbrewer.YlGn[3]);
-
+    t.throws(function(){
+        colorbrewify(data);
+    });
 });
+
+// test('sequential test', function(t) {
+//     t.plan(1);
+
+//     actual = [1,2,3,4,5,6,3,2,3,6,1];
+
+//     t.equal(colorbrewify(actual), colorbrewer.YlGn[3]);
+
+// });
 
 test('binary test', function(t) {
     t.plan(1);
