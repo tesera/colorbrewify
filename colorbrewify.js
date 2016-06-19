ebc5eb5bfc658259b6aee9a1b9eecc125fac0ae7
Yves Richard <yves.richard@tesera.com>
2016-06-19 12:17:27 -0600

    Add qualitative data character

--- colorbrewify.js
+++ colorbrewify.js
@@ -5,11 +5,22 @@ var schemes = require('./schemes.json')
 module.exports = function (values) {
     var scheme = schemes['sequentialSingle'][0];
     var brewer = {
-        binary: 'not implemented',
+        sequential: scale.scaleSequential(chromatic.interpolatePiYG),
         qualitative: scale.scaleOrdinal(chromatic.schemeAccent),
         diverging: scale.scaleSequential(chromatic.interpolatePiYG),
-        sequential: scale.scaleSequential(chromatic.interpolatePiYG)
+        binary: 'not implemented',
     };
 
-    return values.map(brewer['sequential']);
+    var character = 'sequential';
+    var uniqueValues = values.reduce(function (uniques, v) {
+        if (uniques.indexOf(v) === -1) uniques.push(v);
+        return uniques
+    }, []);
+   
+    // if unique values is less than 5% consider values qualitative
+    if (uniqueValues.length / values.length < 0.5) character = 'qualitative';
+
+    console.log('values character is %s', character);
+
+    return values.map(brewer[character]);
 };
--- test/colorbrewerify_test.js
+++ test/colorbrewerify_test.js
@@ -5,33 +5,33 @@ test('sequential values', function(t) {
     var expectedColors = ['rgb(39, 100, 25)', 'rgb(39, 100, 25)', 'rgb(39, 100, 25)', 'rgb(39, 100, 25)', 'rgb(39, 100, 25)', 'rgb(39, 100, 25)', 'rgb(39, 100, 25)', 'rgb(39, 100, 25)', 'rgb(39, 100, 25)', 'rgb(39, 100, 25)', 'rgb(39, 100, 25)' ];
     var values = [1, 2, 3, 4, 5, 6, 3, 2, 3, 6, 1];
     var colors = colorbrewify(values);
-    t.equal(colors, expectedColors);
-    t.end();
-});
-
-test('binary values', function(t) {
-    var expectedColors = [];
-    t.skip('not implemented');
-    var values = [true,false,true,true,false];
-    var colors = colorbrewify(values);
-    t.equal(colors, expectedColors);
+    t.deepLooseEqual(colors, expectedColors);
     t.end();
 });
 
 test('qualitative values', function(t) {
-    var expectedColors = [];
+    var expectedColors = ['#7fc97f', '#beaed4', '#beaed4', '#fdc086', '#beaed4', '#beaed4', '#beaed4', '#beaed4', '#fdc086'];
     t.skip('not implemented');
-    var values = ["a","b","b","c"];
+    var values = ['a', 'b', 'b', 'c', 'b', 'b', 'b', 'b', 'c'];
     var colors = colorbrewify(values);
-    t.equal(colors, expectedColors);
+    t.deepLooseEqual(colors, expectedColors);
     t.end();
 });
 
-test('diverging values', function(t) {
-    var expectedColors = [];
-    t.skip('not implemented');
-    var values = [-1,0,1];
-    var colors = colorbrewify(values);
-    t.equal(colors, expectedColors);
-    t.end();
-});
+// test('diverging values', function(t) {
+//     var expectedColors = [];
+//     t.skip('not implemented');
+//     var values = [-1,0,1];
+//     var colors = colorbrewify(values);
+//     t.equal(colors, expectedColors);
+//     t.end();
+// });
+
+// test('binary values', function(t) {
+//     var expectedColors = [];
+//     t.skip('not implemented');
+//     var values = [true, false, true, true, false];
+//     var colors = colorbrewify(values);
+//     t.equal(colors, expectedColors);
+//     t.end();
+// });
