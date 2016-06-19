207cc12cd43936a8b31372e3b68272946d98af29
Yves Richard <yves.richard@tesera.com>
2016-06-19 16:51:20 -0600

    Add colorbrewify cli

---
+++ cli.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+var fs = require('fs');
+var csv = require('csv');
+var _ = require('lodash');
+var colorbrewify = require('.');
+
+// converts columns to rows and vice versa
+function flip(data) {
+    var flipped = data.reduce(function (flipped, dim) {
+        dim.forEach(function (val, index) {
+            flipped[index] = flipped[index] || [];
+            flipped[index].push(val)
+        });
+        return flipped;
+    }, []);
+
+    return flipped;
+}
+
+program
+    .version('0.0.1')
+    .arguments('<file>')
+    .action(function (file) {
+        fs.readFile(file, 'utf8', function  (err, data) {
+            csv.parse(data, function(err, rows){
+                var columns = flip(rows);
+                var colors = columns.map(colorbrewify);
+                csv.stringify(flip(colors), function (err, output) {
+                    process.stdout.write(output);
+                });
+            });
+        });
+    });
+
+program.parse(process.argv);
