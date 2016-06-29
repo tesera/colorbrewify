7112228877026fcaf001436bd737d10024b1a91f
Yves Richard <yves.richard@tesera.com>
2016-06-29 12:31:35 -0600

    Add --no-headers and --exclude
    If csv add --no-headers flag so the program doesn't splice out the headers. --exclude allows you to not include a column in the clorbrewify process

--- cli.js
+++ cli.js
@@ -19,12 +19,21 @@ function flip(data) {
 program
     .version('0.0.1')
     .arguments('<file>')
+    .option('--exclude [column]', 'comma seperated list of columns to exclude')
+    .option('--no-headers', 'if csv includes no headers')
     .action(function (file) {
         fs.readFile(file, 'utf8', function  (err, data) {
             csv.parse(data, function(err, rows){
+                var headers = [];
+                if (!program.noHeaders) headers = rows.shift();
+                var exclude = program.exclude ? program.exclude.split(',') : [];
                 var columns = flip(rows);
-                var colors = columns.map(colorbrewify);
-                csv.stringify(flip(colors), function (err, output) {
+                var colors = columns.map(function (c, i) {
+                    return exclude.indexOf(i+1+'') > -1 ? c : colorbrewify(c);
+                });
+                var colored = flip(colors);
+                if (!program.noHeaders) colored.unshift(headers);
+                csv.stringify(colored, function (err, output) {
                     process.stdout.write(output);
                 });
             });
